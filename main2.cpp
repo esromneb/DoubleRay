34571038cc335e73493b739f9f5a9342a9f62964
Ben <bjmorse@gmail.com>
2020-03-29 07:10:57 -0700

    cenering objects makes camera work, things are still very broken

--- main2.cpp
+++ main2.cpp
@@ -672,8 +672,8 @@ void setupOrbit(const int _frames) {
         }
 
         // calculate next frame
-        cameraOrigin.rot_x(bump);
-        cameraDir.rot_x(bump);
+        cameraOrigin.rot_y(bump);
+        cameraDir.rot_y(bump);
 
         // load these from the Vec
         originX = cameraOrigin.data[0];
